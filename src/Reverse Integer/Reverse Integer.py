d64b6682a411c472a17e9171108c16b8f5b32303
Bruce Chen <bruce30262@gmail.com>
2014-10-28 02:05:19 +0800

    update Reverse Integer.py

--- src/Reverse Integer/Reverse Integer.py
+++ src/Reverse Integer/Reverse Integer.py
@@ -3,14 +3,9 @@ class Solution:
 	def reverse(self, x):
 		if x < 0:
 			ret = "-"
-			for c in reversed(str(abs(x))):
-				ret += c
-			return int(ret)
-		elif x == 0:
-			return 0
 		else:
 			ret = ""
-			for c in reversed(str(abs(x))):
-				ret += c
-			return int(ret)
-
+		
+		for c in reversed(str(abs(x))):
+			ret += c
+		return int(ret)
